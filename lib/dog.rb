2f9ffbe14cffd42ac638d96bc652183233c97367
Milan Mody <milanm1120@gmail.com>
2021-03-02 01:49:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+
+  def name=(dog_name)           #This method is responsible to taking in an argument of a dogs name and setting it equal to the variable, this_dogs_name
+    @this_dogs_name = dog_name
+  end
+
+  def name                      #This method is responsible for reproting the name
+    @this_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
